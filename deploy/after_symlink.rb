d182c1c1f3179e66787c60422940ab7cf8d664d9
Pasan Chamikara <gwpc92@gmail.com>
2020-10-07 12:07:10 +0530

    sample

--- deploy/after_symlink.rb
+++ deploy/after_symlink.rb
@@ -1,4 +1,4 @@
-on_utilities('sphinx') do
+on_utilities('thinking_sphinx') do
   run "[[ -d #{config.shared_path}/sphinx ]] && ln -nfs #{config.shared_path}/sphinx #{config.current_path}/db/sphinx"
   run "cd #{config.current_path} && RAILS_ENV=#{config.environment} bundle exec rake ts:configure"
 end
